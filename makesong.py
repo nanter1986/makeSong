2ef80b728970e582f70b71faa0c45ed9e60f680f
nanter1986 <nanter1986@gmail.com>
2018-04-23 18:56:26 +0300

    drums

--- makesong.py
+++ makesong.py
@@ -1,5 +1,4 @@
-from EasyMIDI import EasyMIDI,Track,Note,Chord,RomanChord
-from random import choice
+from EasyMIDI import EasyMIDI,Track,Note,Chord,RomanChord from random import choice
 
 easyMIDI = EasyMIDI()
 track1 = Track("acoustic grand pino")
@@ -18,10 +17,11 @@ def createProgression():
     second=['II','IV']
     third=['I','VI']
     fourth=['V','VII']
-    track1.addChord(RomanChord(random.choice(first),4,1 'C',True,100))
-    track1.addChord(RomanChord(random.choice(second),4,1 'C',True,100))
-    track1.addChord(RomanChord(random.choice(third),4,1 'C',True,100))
-    track1.addChord(RomanChord(random.choice(fourth),4,1 'C',True,100))
+    for i in range(0,3):
+        track1.addChord(RomanChord(random.choice(first),4,1 'C',True,100))
+        track1.addChord(RomanChord(random.choice(second),4,1 'C',True,100))
+        track1.addChord(RomanChord(random.choice(third),4,1 'C',True,100))
+        track1.addChord(RomanChord(random.choice(fourth),4,1 'C',True,100))
 
 def addPercussion(progression):
     #drums
