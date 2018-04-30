cf9f2fa443d9ad8254464275fa1f794d1ece8ab1
nanter1986 <nanter1986@gmail.com>
2018-04-30 08:52:47 +0300

    chords are slowly changing

--- makesong.py
+++ makesong.py
@@ -69,28 +69,36 @@ def createProgression():
 
 def addProgression(progression):
     #adds chords to the chord track
-    chord1=RomanChord(progression['progression'][0],4,1,theKey,progression['flavor'][0],100)
-    chord2=RomanChord(progression['progression'][1],4,1,theKey,progression['flavor'][1],100)
-    chord3=RomanChord(progression['progression'][2],4,1,theKey,progression['flavor'][2],100)
-    chord4=RomanChord(progression['progression'][3],4,1,theKey,progression['flavor'][3],100)
-    for i in range(0,2*duration):
+    chord1=RomanChord(progression['progression'][0],4,4,theKey,progression['flavor'][0],100)
+    chord2=RomanChord(progression['progression'][1],4,4,theKey,progression['flavor'][1],100)
+    chord3=RomanChord(progression['progression'][2],4,4,theKey,progression['flavor'][2],100)
+    chord4=RomanChord(progression['progression'][3],4,4,theKey,progression['flavor'][3],100)
+    for i in range(0,duration2):
         print(progression['progression'][0])
-        track1.addChord(chord1)
-        track1.addChord(chord2)
-        track1.addChord(chord3)
-        track1.addChord(chord4)
-    notes1=chord1.getNotes()
-    notes2=chord2.getNotes()
-    notes3=chord3.getNotes()
-    notes4=chord4.getNotes()
-    for n in notes1:
-        print("chord 1:"+n.name)
-    for n in notes2:
-        print("chord 2:"+n.name)
-    for n in notes3:
-        print("chord 3:"+n.name)
-    for n in notes4:
-        print("chord 4:"+n.name)
+        for t1 in range(0,4):
+            track1.addChord(chord1)
+            notes1=chord1.getNotes()
+            print(chord1.getNumeral())
+            for n in notes1:
+                print("chord 1:"+n.name)
+        for t2 in range(0,4):
+            track1.addChord(chord2)
+            notes2=chord2.getNotes()
+            print(chord2.getNumeral())
+            for n in notes2:
+                print("chord 2:"+n.name)
+        for t3 in range(0,4):
+            track1.addChord(chord3)
+            notes3=chord3.getNotes()
+            print(chord3.getNumeral())
+            for n in notes3:
+                print("chord 3:"+n.name)
+        for t4 in range(0,4):
+            track1.addChord(chord4)
+            notes4=chord4.getNotes()
+            print(chord4.getNumeral())
+            for n in notes4:
+                print("chord 4:"+n.name)
 
 def addPercussion(progression):
     #creates drum pattern based on parameter,and adds ut to drum track
